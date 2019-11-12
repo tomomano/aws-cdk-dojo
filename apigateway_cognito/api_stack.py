2dbec34bdb049071606e2c71f4ae0f68ccb0df75
Tomoyuki Mano <tomoyukimano@gmail.com>
2019-11-13 01:02:55 +0900

    WIP

--- apigateway_cognito/api_stack.py
+++ apigateway_cognito/api_stack.py
@@ -18,7 +18,30 @@ class ApigatewayCognitoStack(core.Stack):
             sign_in_type=cognito.SignInType.EMAIL
         )
         cfn_user_pool = user_pool.node.default_child
-        cfn_user_pool.policies = 
+        cfn_user_pool.policies = {
+            "passwordPolicy": {
+                "minimumLength": 8,
+                "requireLowercase": True,
+                "requireNumbers": True,
+                "requireUppercase": True,
+                "requireSymbols": False
+            }
+        }
+        # create pool client
+        pool_client = cognito.CfnUserPoolClient(
+            self, 'testUserPoolClient',
+            user_pool_id=user_pool.user_pool_id,
+            supported_identity_providers=["COGNITO"],
+            generate_secret=False,
+            refresh_token_validity=1,
+            allowed_o_auth_flows_user_pool_client=True,
+            allowed_o_auth_flows=["implicit"],
+            allowed_o_auth_scopes=["email", "openid", "aws.cognito.signin.user.admin"],
+            callback_ur_ls=["http://localhost"],
+            logout_ur_ls=["http://localhost"]
+        )
+        # output some stuff
+        core.CfnOutput(self, "UserPool ID", value=user_pool.user_pool_id)
 
         # create REST API resource
         api = apigw.RestApi(self, 'my_API')
@@ -33,7 +56,7 @@ class ApigatewayCognitoStack(core.Stack):
             type='COGNITO_USER_POOLS',
             identity_source='method.request.header.Authorization',
             rest_api_id=api.rest_api_id,
-            provider_arns=[cognito_arn]
+            provider_arns=[user_pool.user_pool_arn]
         )
 
         # lambda handler
